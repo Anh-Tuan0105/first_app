f41b74d9cbdb8c8b09a0e675d0789025127239d8
unknown <huynhngocanhtuan9a9@gmail.com>
2024-11-16 20:46:39 +0700

    google

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,31 +5,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         home: Scaffold(
             body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'Chào Mừng',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              fontStyle: FontStyle.normal,
+          RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+              children: [
+                TextSpan(text: 'G', style: TextStyle(color: Colors.blue)),
+                TextSpan(text: 'o', style: TextStyle(color: Colors.red)),
+                TextSpan(text: 'o', style: TextStyle(color: Colors.yellow)),
+                TextSpan(text: 'g', style: TextStyle(color: Colors.blue)),
+                TextSpan(text: 'l', style: TextStyle(color: Colors.green)),
+                TextSpan(text: 'e', style: TextStyle(color: Colors.red)),
+              ],
             ),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 25),
             child: TextField(
               decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(40)),
+                  ),
+                  hintText: 'Search',
                   hintStyle: TextStyle(
                     fontStyle: FontStyle.italic,
                   ),
-                  label: Text('Nhập Tên Của Bạn'),
-                  prefixIcon: Icon(Icons.add)),
+                  prefixIcon: Icon(Icons.search)),
             ),
           )
         ],
